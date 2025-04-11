02602786476496b02b99b09efc37562cb0543d3a
1s22s1 <1s22s1@users.noreply.github.com>
2025-04-11 10:11:41 +0900

    :up:

---
+++ 20250411/abc154_c.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n';
+
+int init()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+
+    return 0;
+}
+
+int main()
+{
+    init();
+
+    int N;
+    cin >> N;
+
+    set<int> A;
+
+    for (auto _ : ranges::views::iota(0, N))
+    {
+        int a;
+        cin >> a;
+
+        A.insert(a);
+    }
+
+    if (A.size() == N)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
